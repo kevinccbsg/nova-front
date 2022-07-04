7e1ff7006922e3807329101539c0b90c8419c779
kevinccbsg <kevinccbsg@gmail.com>
2022-07-04 13:17:43 +0200

    feat: add styles to the nominate page

--- src/pages/Nominate/Nominate.tsx
+++ src/pages/Nominate/Nominate.tsx
@@ -4,6 +4,7 @@ import {
 import Main from '../../layouts/Main';
 import Button from '../../components/Button';
 import { nominateValidation } from '../../validators';
+import style from './Nominate.module.scss';
 
 const INIT_VALUES = {
   email: '',
@@ -14,8 +15,8 @@ const INIT_VALUES = {
 
 const Nominate = () => (
   <Main>
-    <section>
-      <h1>Nominate a candidate</h1>
+    <section className={style.container}>
+      <h1 className={style.header}>Nominate a candidate</h1>
       <Formik
         initialValues={INIT_VALUES}
         validationSchema={nominateValidation()}
@@ -23,29 +24,31 @@ const Nominate = () => (
       >
         {({ isSubmitting }) => (
           <Form>
-            <div>
+            <div className={style.inputContainer}>
               <label htmlFor="email">email</label>
               <Field type="email" name="email" id="email" />
               <ErrorMessage name="email" component="div" />
             </div>
-            <div>
+            <div className={style.inputContainer}>
               <label htmlFor="description">description</label>
               <Field as="textarea" name="description" id="description" cols={30} rows={10} />
               <ErrorMessage name="description" component="div" />
             </div>
-            <div>
+            <div className={style.inputContainer}>
               <label htmlFor="involvement">involvement</label>
               <Field type="range" name="involvement" id="involvement" step={1} min={0} max={10} />
               <ErrorMessage name="involvement" component="div" />
             </div>
-            <div>
+            <div className={style.inputContainer}>
               <label htmlFor="talent">talent</label>
               <Field type="range" name="talent" id="talent" step={1} min={0} max={10} />
               <ErrorMessage name="talent" component="div" />
             </div>
-            <Button type="submit" theme="primary" disabled={isSubmitting}>
-              Submit
-            </Button>
+            <div>
+              <Button type="submit" theme="primary" disabled={isSubmitting}>
+                Nominate
+              </Button>
+            </div>
           </Form>
         )}
       </Formik>
