014f158cc276392397782e0a7ed33f51a49a3ae5
kevinccbsg <kevinccbsg@gmail.com>
2022-07-04 12:05:55 +0200

    feat: include formik to handle nomination form

--- src/pages/Nominate/Nominate.tsx
+++ src/pages/Nominate/Nominate.tsx
@@ -1,6 +1,39 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 const Nominate = () => (
   <section>
-    Add form
+    <Formik
+      initialValues={{
+        email: '',
+        description: '',
+        involvement: 0,
+        talent: 0,
+      }}
+      onSubmit={() => console.log('submit')}
+    >
+      <Form>
+        <div>
+          <label htmlFor="email">email</label>
+          <Field type="email" name="email" id="email" />
+          <ErrorMessage name="email" component="div" />
+        </div>
+        <div>
+          <label htmlFor="description">description</label>
+          <Field as="textarea" name="description" id="description" cols={30} rows={10} />
+          <ErrorMessage name="description" component="div" />
+        </div>
+        <div>
+          <label htmlFor="involvement">involvement</label>
+          <Field type="range" name="involvement" id="involvement" step={1} min={0} max={10} />
+          <ErrorMessage name="email" component="div" />
+        </div>
+        <div>
+          <label htmlFor="talent">talent</label>
+          <Field type="range" name="talent" id="talent" step={1} min={0} max={10}  />
+          <ErrorMessage name="email" component="div" />
+        </div>
+      </Form>
+    </Formik>
   </section>
 );
 
