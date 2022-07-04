2464ea15cd974e4c3e83432426982fc6cd15146b
kevinccbsg <kevinccbsg@gmail.com>
2022-07-04 19:45:23 +0200

    fix: missing env variable

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import i18nResources from 'vite-plugin-i18n-resources'
 import { i18nextScanner } from 'vite-plugin-i18next-scanner'
 import { resolve } from 'path';
+import EnvironmentPlugin from 'vite-plugin-environment'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    EnvironmentPlugin(['VITE_API_URL']),
     i18nResources({
       path: resolve(__dirname, 'src/locales'),
     }),
